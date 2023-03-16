8b8939658d0a483dc0841fdaf7a882dd2a124c32
gustavo <gusgurtavo@gmail.com>
2023-03-16 11:11:59 -0300

    refatorando as funções

--- script.py
+++ script.py
@@ -7,28 +7,37 @@ def calculo_media(numeros):
     return u
 
 
+def calculo_desvios(numeros, u):
+    lista_desvios = []
+    for numero in numeros:
+        lista_desvios.append((numero-u)**2)
+    return lista_desvios
+
+
+def variancia_desvio(us):
+    return round(us**(1/2), 2)
+
+
 def main():
     os.system('clear')
     numeros = []
     lista_desvios = []
     while True:
+        # os.system('clear')
         try:
             numeros.append(float(input("numero: ")))
             os.system('clear')
-            u = calculo_media(numeros)
-            print("Numeros", numeros)
-            print("Média: ", u)
-            lista_desvios = []
-            for numero in numeros:
-                lista_desvios.append((numero-u)**2)
-
-            print("Desvio pradrão-> ", lista_desvios)
-
-            us = calculo_media(lista_desvios)
-            print(round(us**(1/2), 2))
         except ValueError:
             os.system('clear')
-            print("somante numeros")
+            print("\n\n\n--> somante numeros <--\n\n\n")
+
+        u = calculo_media(numeros)
+        print("Numeros", numeros)
+        print("Média: ", u)
+        lista_desvios = calculo_desvios(numeros, u)
+
+        print("Desvio pradrão-> ", lista_desvios)
+        print('{}'.format(variancia_desvio(calculo_media(lista_desvios))))
 
 
 main()
